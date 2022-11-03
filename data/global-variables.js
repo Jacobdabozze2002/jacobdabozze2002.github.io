ec0f736b5643a39aa4dfb149c181298192475c46
Jacob <emulator.elite@gmail.com>
2022-11-03 14:31:33 +0100

    Bilder für Titelseite implementiert

--- data/global-variables.js
+++ data/global-variables.js
@@ -1,9 +1,9 @@
 const default_pages =
     [
         ["naiv", true, "/data/images/level1.PNG"],
-        ["bm", false, "/data/images/level1.PNG"],
-        ["kmp", false, "/data/images/level1.PNG"],
-        ["suffix", false, "/data/images/level1.PNG"],
+        ["bm", false, "/data/images/level2.PNG"],
+        ["kmp", false, "/data/images/level3.PNG"],
+        ["suffix", false, "/data/images/level4.PNG"],
         "concept"
     ];  // [<page>, <is_unlocked>, <picture>]
 if (!varExists("pages")) storeVar(default_pages, "pages");
