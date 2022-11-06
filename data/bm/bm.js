45df5275e62f88164dad498326deb1158d43c0af
Jacob <emulator.elite@gmail.com>
2022-11-06 16:06:32 +0100

    fix

--- data/bm/bm.js
+++ data/bm/bm.js
@@ -1,3 +1,6 @@
+// DEFs
+const pages = restoreVar("pages");
+
 // main container
 const container = new JF_Window().
 styleClass("standard_jf_window_style");
@@ -18,4 +21,30 @@ sizingByChildren(true, ["5vw", "8vw"]).
 addChildrenByText("sip").
 attachTo(text, "below-left");
 
- 
+// master Text
+const master = new JF_Element(container).
+styleClass("master").
+alignTo("top-center")
+
+// M movement
+let index = 0;
+onkeydown = event =>
+{
+    switch (event.key) {
+        case "ArrowRight":
+            if (index + 1 <= text.getChildren().length - search.getChildren().length) 
+            {
+                search.moveBy("6vw");
+                ++index;
+            }
+            break;
+            
+        case "ArrowLeft":
+            if (index > 0) 
+            {
+                search.moveBy("-6vw");  
+                --index;
+            }
+            break;
+    }
+}
