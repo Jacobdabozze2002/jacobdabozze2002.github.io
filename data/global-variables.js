efa09592197be02d2fc90de74eeae45e030a62fc
Jacob <emulator.elite@gmail.com>
2022-12-05 16:12:48 +0100

    Bug Fix 2

--- data/global-variables.js
+++ data/global-variables.js
@@ -1,8 +1,8 @@
 const default_pages =
     [
         ["naiv", true, "/data/images/level1.PNG"],
-        ["bm", false, "/data/images/level2.PNG"],
         ["kmp", false, "/data/images/level3.PNG"],
+        ["bm", false, "/data/images/level2.PNG"],
         ["suffix", false, "/data/images/level4.PNG"],
         "concept"
     ];  // [<page>, <is_unlocked>, <picture>]
