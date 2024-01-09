80f562d56f3b0bb4f4dbc7c2657ec1128fa755f3
Jacobdabozze2002 <92543896+Jacobdabozze2002@users.noreply.github.com>
2024-01-09 19:21:15 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,47 @@
+const template = document.getElementById("template");
+const title = document.getElementById("title");
+const details = document.getElementById("details");
+const turret = document.getElementById("turret");
+const hull = document.getElementById("hull");
+const paint = document.getElementById("paint");
+const protection = document.getElementById("protection");
+const rewards = document.getElementById("rewards");
+const notes = document.getElementById("notes");
+
+[title, details, turret, hull, paint, protection, rewards, notes].forEach(input =>
+{
+    input.addEventListener("keyup", _ =>
+    {
+        reload_template();
+    })
+})
+
+function copy()
+{
+    navigator.clipboard.writeText(template.value);
+}
+
+function reload_template()
+{
+    template.value = "";
+    template.value += `# ${title.value || "no title"}\n** **\n`;
+
+    template.value += `**Details:**\n`;
+    template.value += `${details.value || "no details"}\n\n`;
+
+    template.value += `**Equipment:**\n`;
+    template.value += `Turret: ${turret.value || "no restriction"}\n`;
+    template.value += `Hull: ${hull.value || "no restriction"}\n`;
+    template.value += `Paint: ${paint.value || "no restriction"}\n`;
+    template.value += `Protection: ${protection.value || "no restriction"}\n\n`;
+
+    template.value += `**Rewards:**\n`;
+    template.value += `${rewards.value || "no winners"}\n\n`;
+
+    template.value += `**Notes:**\n`;
+    template.value += `${notes.value || "no notes"}\n\n`;
+
+}
+
+reload_template();
+
